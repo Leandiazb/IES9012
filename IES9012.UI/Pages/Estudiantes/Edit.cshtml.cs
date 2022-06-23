c8e4b4e5569a431c91e8eae19eea186bbbebd6ad
Leandiazb <80428209+Leandiazb@users.noreply.github.com>
2022-06-23 15:37:37 -0300

    Add files via upload

--- IES9012.UI/Pages/Estudiantes/Edit.cshtml.cs
+++ IES9012.UI/Pages/Estudiantes/Edit.cshtml.cs
@@ -23,55 +23,18 @@ namespace IES9012.UI.Pages.Estudiantes
         [BindProperty]
         public Estudiante Estudiante { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null || _context.Estudiantes == null)
-            {
-                return NotFound();
-            }
-
-            var estudiante =  await _context.Estudiantes.FirstOrDefaultAsync(m => m.EstudianteId == id);
-            if (estudiante == null)
-            {
-                return NotFound();
-            }
-            Estudiante = estudiante;
-            return Page();
-        }
-
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var emptyStudient = new Estudiante();
+            if (await TryUpdateModelAsync<Estudiante>(
+            emptyStudient,
+            "estudiante", //Prefijo para el valor del formulario.
+            s => s.Nombre, s => s.Apellido, s => s.FechaInscripcion))
             {
-                return Page();
-            }
-
-            _context.Attach(Estudiante).State = EntityState.Modified;
-
-            try
-            {
+                _context.Estudiantes.Add(emptyStudient);
                 await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!EstudianteExists(Estudiante.EstudianteId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
-        }
-
-        private bool EstudianteExists(int id)
-        {
-          return (_context.Estudiantes?.Any(e => e.EstudianteId == id)).GetValueOrDefault();
+            return Page();
         }
     }
-}
